749e2c4d038f8422cc62277676e627dd37b5efde
Kung0301 <kung.positive.mindset@gmail.com>
2024-05-21 22:17:15 +0700

    type code for exercise 4
    finish

---
+++ Exercise4_Panot_Sittikarnka.py
@@ -0,0 +1,9 @@
+FD = 80
+GB = 82.5
+ITC = 90
+CP = 98
+print("--- You Score ---")
+print("Foundation English : ", FD)
+print("General Business : ", GB)
+print("Introduction to Computer : ", ITC)
+print("Computer Programming : ", CP)
