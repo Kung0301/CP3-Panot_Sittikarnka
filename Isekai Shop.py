7dc8bddd97db99e9d0471b674c1c826ef685f25f
Kung0301 <kung.positive.mindset@gmail.com>
2024-05-30 18:40:16 +0700

    Update Isekai Shop.py

--- Isekai Shop.py
+++ Isekai Shop.py
@@ -1,21 +1,26 @@
-username = input("Username :")
-password = input("Password :")
-if username == "Kirito" and password == "Tepza007" :
-    print("Welcome to Isekai Shop")
-    print("We have :")
-    potion = int(input("Potion 20 TZ :"))
-    price_I = 20 * potion
-    sword = int(input("Sword 400 TZ :"))
-    price_II = 400 * sword
-    skill = int(input("Star Burst Stream(Skill) 2000 TZ :"))
-    price_III = 2000 * skill
-    total = price_I + price_II + price_III
-    member = input("Do you have Tepza member card :")
-    if member == "Yes" :
-        print("Tatal price :", (total - (total * 10 / 100)), "TZ")
-        print("Thank you")
-    elif member == "No" :
-        print("Total price :", (total / 1), "TZ")
-        print("Thank you")
-else :
-    print("Username or password is incorrect")
+def login() :
+    username = input("Username :")
+    password = input("Password :")
+    if username == "Kirito" and password == "Tepza007":
+        return show_menu()
+    else :
+        return login()
+def show_menu() :
+    print("Welcome")
+    print("1) Vat Calculator")
+    print("2) Price Calculator")
+    return menu_select()
+def menu_select() :
+    userselected = int(input(">>"))
+    if userselected == 2 :
+        return price_cal()
+def vat_cal(total_price) :
+    vat = 7
+    result = total_price+(total_price*vat/100)
+    return result
+def price_cal() :
+    price_1 = int(input("First product price :"))
+    price_2 = int(input("Second product price :"))
+    return vat_cal(price_1+price_2)
+
+print(login())
